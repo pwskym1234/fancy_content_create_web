3367f34f9a5b1bd5653d7b60864c71cf4d509bcd
pwskym1234 <pwskym@snu.ac.kr>
2024-09-04 12:37:24 +0900

    controller update

--- lib/feature/home/logic/controller.dart
+++ lib/feature/home/logic/controller.dart
@@ -18,7 +18,7 @@ final qaListControllerProvider = StateNotifierProvider<
 );
 final introControllerProvider = Provider((ref) => TextEditingController());
 final contentIDControllerProvider = Provider((ref) => TextEditingController());
-final statusProvider = StateProvider<String>((ref) => 'INACTIVE');
+final statusProvider = StateProvider<String>((ref) => 'ACTIVE');
 final categoryProvider = StateProvider<String>((ref) => 'LOVE');
 final averageRatingProvider = StateProvider<double>((ref) => 4.8);
 final wordCountProvider = StateProvider<int>((ref) => 400);
--- lib/feature/home/logic/update_content_logic.dart
+++ lib/feature/home/logic/update_content_logic.dart
@@ -8,6 +8,126 @@ import 'package:universal_html/html.dart' as html;
 
 import 'package:flutter/material.dart';
 
+// Future<void> updateContentLogic({
+//   required BuildContext context,
+//   required WidgetRef ref,
+// }) async {
+//   final contentTitleController = ref.read(contentTitleControllerProvider);
+//   final introController = ref.read(introControllerProvider);
+//   final qaController = ref.read(qaListControllerProvider);
+//   // final contentIDController = ref.read(newContentIdFromResponseProvider);
+//   final contentIDController = ref.watch(contentIDControllerProvider);
+//   final categoryController = ref.read(categoryProvider);
+//   final statusController = ref.read(statusProvider);
+//   final apiService = ref.read(apiServiceProvider);
+//   final thumbnail = ref.read(thumbnailProvider);
+//   final description = ref.read(descriptionProvider);
+//   final firstQuestionList = ref.read(selectedQuestionIdsProvider);
+//   final preview = ref.read(previewProvider);
+//   final averageRating = ref.read(averageRatingProvider);
+//   final wordCount = ref.read(wordCountProvider);
+//   String qaText = qaController.text;
+//   String introText = introController.text;
+//   String titleText = contentTitleController.text;
+//   int? contentID = int.tryParse(contentIDController.text);
+//   // int? contentID = contentIDController;
+//   String? status =
+//       statusController.isNotEmpty == true ? statusController : null;
+//   String? category =
+//       categoryController.isNotEmpty == true ? categoryController : null;
+
+//   // if (contentID == null) {
+//   //   if (context.mounted) {
+//   //     ScaffoldMessenger.of(context).showSnackBar(
+//   //       const SnackBar(content: Text('Content ID must be a valid integer')),
+//   //     );
+//   //   }
+//   //   return;
+//   // }
+
+//   List<String> newFormattedQAList = splitText(qaText);
+//   newFormattedQAList = newFormattedQAList.map((qa) {
+//     return qa.replaceAll(RegExp(r'^\(\d+\)'), '').trim();
+//   }).toList();
+
+//   try {
+//     debugPrint('Updating content with contentID: $contentID');
+
+//     Response contentResponse = await apiService.updateContent(
+//       contentID!,
+//       thumbnail: thumbnail,
+//       description: description,
+//       status: status,
+//       category: category,
+//       previews: preview,
+//       introText: introText.isNotEmpty ? introText : null,
+//       title: titleText.isNotEmpty ? titleText : null,
+//       averageRating: averageRating,
+//       wordCount: wordCount,
+//       firstQuestionList:
+//           firstQuestionList.isNotEmpty ? firstQuestionList : null,
+//     );
+//     int newContentIdFromResponse = contentResponse.data['id'];
+//     ref
+//         .read(newContentIdFromResponseProvider.notifier)
+//         .updateContentId(newContentIdFromResponse);
+//     debugPrint('Content created with ID: $newContentIdFromResponse');
+
+//     Map<int, int> questionIds = {};
+//     Map<String, int> questionTextToIdMap = {};
+
+//     for (String qa in newFormattedQAList) {
+//       if (qa.startsWith('Q:')) {
+//         String questionText = qa.substring(2).trim();
+//         debugPrint('Creating question: $questionText');
+//         Response questionResponse = await apiService.createQuestion(
+//             newContentIdFromResponse, questionText);
+//         int questionId = questionResponse.data['id'];
+//         String questionTextFromResponse = questionResponse.data['text'].trim();
+//         questionTextToIdMap[questionTextFromResponse] = questionId;
+//         debugPrint('Question created with ID: $questionId');
+//         questionIds[newFormattedQAList.indexOf(qa)] = questionId;
+//       }
+//     }
+
+//     for (String qa in newFormattedQAList) {
+//       if (qa.startsWith('A:')) {
+//         String answerText = qa.substring(2).trim();
+//         int answerIndex = newFormattedQAList.indexOf(qa);
+//         String previousQuestionText =
+//             findPreviousQuestionText(answerIndex, newFormattedQAList);
+//         String nextQuestionText =
+//             findNextQuestionText(answerIndex, newFormattedQAList);
+//         int previousQuestionId = questionTextToIdMap[previousQuestionText] ?? 0;
+//         int? nextQuestionId = questionTextToIdMap[nextQuestionText];
+
+//         debugPrint('Creating answer: $answerText');
+//         debugPrint('Previous question ID: $previousQuestionId');
+//         debugPrint('Next question ID: $nextQuestionId');
+//         debugPrint('content ID: $newContentIdFromResponse');
+//         await apiService.createAnswer(
+//             newContentIdFromResponse, answerText, previousQuestionId,
+//             nextQuestionId: nextQuestionId);
+//         debugPrint('Answer created successfully');
+//       }
+//     }
+
+//     if (context.mounted) {
+//       ScaffoldMessenger.of(context).showSnackBar(
+//         const SnackBar(content: Text('Updated successfully')),
+//       );
+//     }
+//     debugPrint('Updated successfully');
+//   } catch (e) {
+//     debugPrint('Error occurred: $e');
+//     if (context.mounted) {
+//       ScaffoldMessenger.of(context).showSnackBar(
+//         SnackBar(content: Text('Error submitting Q&A: $e')),
+//       );
+//     }
+//   }
+// }
+
 Future<void> updateContentLogic({
   required BuildContext context,
   required WidgetRef ref,
@@ -15,8 +135,8 @@ Future<void> updateContentLogic({
   final contentTitleController = ref.read(contentTitleControllerProvider);
   final introController = ref.read(introControllerProvider);
   final qaController = ref.read(qaListControllerProvider);
-  // final contentIDController = ref.read(newContentIdFromResponseProvider);
-  final contentIDController = ref.watch(contentIDControllerProvider);
+  final contentIDController = ref.read(newContentIdFromResponseProvider);
+  // final contentIDController = ref.watch(contentIDControllerProvider);
   final categoryController = ref.read(categoryProvider);
   final statusController = ref.read(statusProvider);
   final apiService = ref.read(apiServiceProvider);
@@ -29,7 +149,8 @@ Future<void> updateContentLogic({
   String qaText = qaController.text;
   String introText = introController.text;
   String titleText = contentTitleController.text;
-  int? contentID = int.tryParse(contentIDController.text);
+  // int? contentID = int.tryParse(contentIDController.text);
+  int? contentID = contentIDController;
   String? status =
       statusController.isNotEmpty == true ? statusController : null;
   String? category =
