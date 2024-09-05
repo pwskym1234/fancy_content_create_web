65ec3d36ae54064cc4bb6d02a04a7a562eb17aac
pwskym1234 <pwskym@snu.ac.kr>
2024-09-05 12:48:12 +0900

    자동으로 firstquestionlist전달되게

--- lib/feature/update_page/logic/updateContentLogicv2.dart
+++ lib/feature/update_page/logic/updateContentLogicv2.dart
@@ -18,26 +18,14 @@ Future<void> updateContentLogicv2({
   final firstQuestionList = ref.read(selectedQuestionIdsProvider);
   String qaText = qaController.text;
   int? contentID = int.tryParse(contentIDController.text);
-
+  List<Map<String, dynamic>> questions = [];
+  List<Map<String, dynamic>> answers = [];
   List<String> newFormattedQAList = splitText(qaText);
   newFormattedQAList = newFormattedQAList.map((qa) {
     return qa.replaceAll(RegExp(r'^\(\d+\)'), '').trim();
   }).toList();
 
   try {
-    debugPrint('Updating content with contentID: $contentID');
-
-    Response contentResponse = await apiService.updateContent(
-      contentID!,
-      firstQuestionList:
-          firstQuestionList.isNotEmpty ? firstQuestionList : null,
-    );
-    int newContentIdFromResponse = contentResponse.data['id'];
-    ref
-        .read(newContentIdFromResponseProvider.notifier)
-        .updateContentId(newContentIdFromResponse);
-    debugPrint('Content ID: $newContentIdFromResponse');
-
     Map<int, int> questionIds = {};
     Map<String, int> questionTextToIdMap = {};
 
@@ -45,8 +33,8 @@ Future<void> updateContentLogicv2({
       if (qa.startsWith('Q:')) {
         String questionText = qa.substring(2).trim();
         debugPrint('Creating question: $questionText');
-        Response questionResponse = await apiService.createQuestion(
-            newContentIdFromResponse, questionText);
+        Response questionResponse =
+            await apiService.createQuestion(contentID!, questionText);
         int questionId = questionResponse.data['id'];
         String questionTextFromResponse = questionResponse.data['text'].trim();
         questionTextToIdMap[questionTextFromResponse] = questionId;
@@ -74,12 +62,187 @@ Future<void> updateContentLogicv2({
         debugPrint('Previous question ID: $previousQuestionId');
         debugPrint('Next question ID: $nextQuestionId');
         await apiService.createAnswer(
-            newContentIdFromResponse, answerText, previousQuestionId,
+            contentID!, answerText, previousQuestionId,
             nextQuestionId: nextQuestionId);
         debugPrint('Answer created successfully');
       }
     }
 
+    // if (context.mounted) {
+    //   ScaffoldMessenger.of(context).showSnackBar(
+    //     const SnackBar(content: Text('Updated successfully')),
+    //   );
+    // }
+    // debugPrint('Updated successfully');
+  } catch (e) {
+    debugPrint('Error occurred: $e');
+    if (context.mounted) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error submitting Q&A: $e')),
+      );
+    }
+  }
+
+  int questionOffset = 0;
+  bool questionHasMore = true;
+  while (questionHasMore) {
+    try {
+      debugPrint('$contentIDController');
+      Response questionResponse = await apiService.listQuestionForAdmin(
+          contentID ?? 0, questionOffset, 100);
+
+      List<Map<String, dynamic>> fetchedQuestions =
+          List<Map<String, dynamic>>.from(questionResponse.data);
+      questions.addAll(fetchedQuestions);
+
+      debugPrint("Questions fetched successfully: $fetchedQuestions");
+
+      if (fetchedQuestions.length < 100) {
+        questionHasMore = false;
+      } else {
+        questionOffset += 100;
+      }
+    } catch (e) {
+      debugPrint('Error occurred while fetching questions: $e');
+      if (context.mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Error fetching questions: $e')),
+        );
+      }
+      return;
+    }
+  }
+
+  // Fetch all answers with pagination
+  int answerOffset = 0;
+  bool answerHasMore = true;
+  while (answerHasMore) {
+    try {
+      Response answerResponse = await apiService.listAnswerForAdmin(
+          contentID ?? 0, answerOffset, 100);
+
+      // Directly use the response data as a List
+      List<Map<String, dynamic>> fetchedAnswers =
+          List<Map<String, dynamic>>.from(answerResponse.data);
+      answers.addAll(fetchedAnswers);
+
+      debugPrint("Answers fetched successfully: $fetchedAnswers");
+
+      if (fetchedAnswers.length < 100) {
+        answerHasMore = false;
+      } else {
+        answerOffset += 100;
+      }
+    } catch (e) {
+      debugPrint('Error occurred while fetching answers: $e');
+      if (context.mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Error fetching answers: $e')),
+        );
+      }
+      return;
+    }
+  }
+
+  try {
+    Map<int, Map<dynamic, dynamic>> answerQuestionMap = {};
+
+    for (var answer in answers) {
+      answerQuestionMap[answer['id']] = {
+        'answer': answer['text'],
+        'question_text': '',
+        'question_id': '',
+        'next_question_text': '',
+        'next_question_id': ''
+      };
+
+      debugPrint(
+          "Processing answer ID: ${answer['id']}, Text: ${answer['text']}");
+
+      for (var question in questions) {
+        int questionId = answer['question_id'];
+        int? nextQuestionId = answer['next_question_id'];
+
+        if (question['id'] == questionId) {
+          answerQuestionMap[answer['id']]?['question_text'] = question['text'];
+          answerQuestionMap[answer['id']]?['question_id'] = question['id'];
+          debugPrint(
+              "    Matched question ID: $questionId, Text: ${question['text']}");
+        }
+        if (nextQuestionId != null && question['id'] == nextQuestionId) {
+          answerQuestionMap[answer['id']]?['next_question_text'] =
+              question['text'];
+          answerQuestionMap[answer['id']]?['next_question_id'] = question['id'];
+          debugPrint(
+              "    Matched next question ID: $nextQuestionId, Text: ${question['text']}");
+        }
+      }
+    }
+
+    debugPrint("Final answerQuestionMap: $answerQuestionMap");
+    ref
+        .read(fetchedAnswerQuestionMapProvider.notifier)
+        .updateFetchedAnswerQuestionMap(answerQuestionMap.values.toList());
+    debugPrint(
+        "Final answerQuestionMap: ${ref.read(fetchedAnswerQuestionMapProvider)}");
+    if (context.mounted) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Fetched successfully')),
+      );
+    }
+    debugPrint("QA Data set to state");
+  } catch (e) {
+    debugPrint('Error occurred while combining QA data: $e');
+    if (context.mounted) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error fetching QA data: $e')),
+      );
+    }
+  }
+
+  try {
+    final fetchedData = ref.watch(fetchedAnswerQuestionMapProvider);
+
+    for (var qa in fetchedData) {
+      ref.watch(currentGroupProvider.notifier).addQAData(qa);
+      if (qa['next_question_id'] == '' && qa['next_question_text'] == '') {
+        ref
+            .watch(groupedQADataProvider.notifier)
+            .addGroupedQAData(ref.watch(currentGroupProvider));
+        ref.watch(currentGroupProvider.notifier).clearQAData();
+      }
+    }
+
+    if (ref.watch(currentGroupProvider).isNotEmpty) {
+      ref
+          .watch(groupedQADataProvider.notifier)
+          .addGroupedQAData(ref.watch(currentGroupProvider));
+    }
+
+    debugPrint("Received qaData: $fetchedData");
+
+    debugPrint(
+        "Number of created groups: ${ref.watch(groupedQADataProvider).length}");
+  } catch (e) {}
+
+  try {
+    debugPrint('Updating content with contentID: $contentID');
+    List<int> getFirstQuestionIds(WidgetRef ref) {
+      List<int> firstQuestionIds = [];
+      for (var group in ref.watch(groupedQADataProvider)) {
+        var qa = group.first;
+        firstQuestionIds.add(qa['question_id']);
+      }
+
+      return firstQuestionIds;
+    }
+
+    List<int> firstQuestionList = getFirstQuestionIds(ref);
+    await apiService.updateContent(
+      contentID!,
+      firstQuestionList:
+          firstQuestionList.isNotEmpty ? firstQuestionList : null,
+    );
     if (context.mounted) {
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text('Updated successfully')),
@@ -90,7 +253,7 @@ Future<void> updateContentLogicv2({
     debugPrint('Error occurred: $e');
     if (context.mounted) {
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Error submitting Q&A: $e')),
+        SnackBar(content: Text('Error updating: $e')),
       );
     }
   }
